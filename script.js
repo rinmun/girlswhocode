7bf81941d71c1dbed5c24da7e3d31bc44842c0db
Glitch (changeable-luxurious-starflower) <none>
2020-08-29 22:12:05 +0000

    🏇🌃 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,9 @@
+function functionApply() {
+  event.preventDefault();
+  document.getElementById("applymessage").innerHTML = "Thank you for your submission! You will contacted soon via email. If you are accepted, please head to the find a mentor tab";
+}
+
+function functionMentor() {
+  event.preventDefault();
+  document.getElementById("mentormessage").innerHTML = "Thank you for your submission! You will contacted soon via email by your mentor";
+}
